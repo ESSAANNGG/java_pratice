ab9fa7f8d0ae017fb94a4e6785d6d8bcb2f8f3a5
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-05-17 20:38:06 +0900

    자바 메소드 공부

--- src/com/company/static2.java
+++ src/com/company/static2.java
@@ -6,12 +6,11 @@ package com.company;
 * 클래스에 존재하지만 클래스에 속하지않은 static 변수
 * */
  class static2 {
-    static int instNum = 0;
+    static int instNum = 100; //클래스 변수
 
     static2() {
         instNum++;
         System.out.println("인스턴스 생성 : " + instNum);
-
     }
 }
     class ClassVar{
@@ -19,6 +18,13 @@ package com.company;
             static2 cnt1 = new static2();
             static2 cnt2 = new static2();
             static2 cnt3 = new static2();
+        }
+    }
+
+    class onlyCLassnoInstanse{
+        public static void main(String[] args){
+            static2.instNum -=25;
+            System.out.println(static2.instNum);
         }
     }
 
